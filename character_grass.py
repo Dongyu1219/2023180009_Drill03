63efb040cbddb9a61655297bf8a8617f9e214aaf
donggyukim@gram <1219kdk@gmail.com>
2024-09-24 09:53:33 +0900

    프린트 통해서 정상 루프 구조가 잘 실행됨.

--- character_grass.py
+++ character_grass.py
@@ -2,10 +2,14 @@ from pico2d import *
 
 #실행 가능한 큰 뻐대를 먼저 만든다.
 #실행가능하다면 커밋한다.
+#무한 루프 맞는지 확인한다.
+#실행 된다면 커밋한다.
 def run_rectangle():
+    print('RECTANGLE')
     pass
 
 def run_circle():
+    print('CIRCLE')
     pass
 
 
@@ -19,7 +23,7 @@ while True:
     #탑다운 설계(하향식 설계)
     #고수: 함수 호출 -> 함수 정의
     #하수: 함수 정의 -> 함수 호출
-    run_rectangle()
     run_circle()
+    run_rectangle()
     
 close_canvas()
