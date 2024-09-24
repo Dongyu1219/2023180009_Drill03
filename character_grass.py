b8078bfba5a08b2e330c0d35c4b76236805e62c6
donggyukim@gram <1219kdk@gmail.com>
2024-09-24 10:01:55 +0900

    캐릭터 출력

--- character_grass.py
+++ character_grass.py
@@ -1,23 +1,24 @@
 from pico2d import *
 
-#실행 가능한 큰 뻐대를 먼저 만든다.
-#실행가능하다면 커밋한다.
-#무한 루프 맞는지 확인한다.
-#실행 된다면 커밋한다.
+open_canvas()
+
+# fill here
+grass = load_image('grass.png')
+boy = load_image('character.png')
+
 def run_rectangle():
     print('RECTANGLE')
     pass
 
 def run_circle():
     print('CIRCLE')
+
+    clear_canvas_now()
+    boy.draw_now(400, 300)
+    delay(1)
+
+    
     pass
-
-
-open_canvas()
-
-# fill here
-grass = load_image('grass.png')
-boy = load_image('character.png')
 
 while True:
     #탑다운 설계(하향식 설계)
@@ -25,5 +26,6 @@ while True:
     #하수: 함수 정의 -> 함수 호출
     run_circle()
     run_rectangle()
+    break
     
 close_canvas()
