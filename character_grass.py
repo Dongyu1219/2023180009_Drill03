fe7e168e6ecbfc6367487cf364cf068a610cf0ea
donggyukim@gram <1219kdk@gmail.com>
2024-09-24 11:16:10 +0900

    최종 제출

--- character_grass.py
+++ character_grass.py
@@ -7,10 +7,28 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+
 def draw_boy(x, y):
     clear_canvas_now()
     boy.draw_now(x, y)
     delay(0.001)
+
+def run_right_side():
+    tx, ty= 400, 700
+    while(ty<0):
+        draw_boy(tx, ty)
+        tx= tx+2
+        ty= ty-2
+
+def run_left_side():
+    tx, ty= 0, 0
+    while True: 
+        draw_boy(tx, ty)
+        tx= tx+2
+        ty= ty+2
+
+def run_left_side():
+    print('left')
     
 def run_top():
     print('TOP')
@@ -58,6 +76,38 @@ def run_circle():
 
     pass
 
+def run_triangle():
+    print('TRINAGLE')
+    tx, ty = 0, 0
+    #run_right_side()
+    #run_bottom()
+    #run_left_side()
+    while(tx<300):
+        clear_canvas_now()
+        boy.draw_now(400+tx, 90)
+        tx = tx+2
+        delay(0.01)
+
+    while(tx >0 and ty< 600):
+        clear_canvas_now()
+        boy.draw_now(400+tx, 90+ty)
+        tx= tx-2
+        ty= ty+math.tan(60/180*math.pi)
+        delay(0.01)
+        
+    while(tx > -400 and ty >0):
+        clear_canvas_now()
+        boy.draw_now(400+tx, 90+ty)
+        tx= tx-2
+        ty= ty-math.tan(60/180*math.pi)
+        delay(0.01)
+        
+    while(tx<0):
+        clear_canvas_now()
+        boy.draw_now(tx + 400, 90)
+        tx+=2
+        delay(0.01)
+        
 while True:
     #탑다운 설계(하향식 설계)
     #고수: 함수 호출 -> 함수 정의
@@ -65,6 +115,6 @@ while True:
     run_circle()
     run_rectangle()
     run_triangle()
-    break
+    
     
 close_canvas()
